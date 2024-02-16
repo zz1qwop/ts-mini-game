56f8b15db5e5a21aa722f6ed7cb099a28b82912c
zz1qwop <84325395+zz1qwop@users.noreply.github.com>
2024-02-16 16:05:48 +0900

    Feat: Start 화면에서 제시된 초성 변경하기

--- src/Components/Start.tsx
+++ src/Components/Start.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useInitial } from '../context/InitialContext';
 
 type Mode = 'start' | 'game' | 'end';
 
@@ -7,16 +8,28 @@ type StartProps = {
 };
 
 function Start({ handleFlag }: StartProps) {
+  const { initial, word, setWord } = useInitial();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setWord([
+      initial[Math.floor(Math.random() * initial.length)],
+      initial[Math.floor(Math.random() * initial.length)],
+    ]);
+  };
   return (
-    <div className="absolute w-full h-full bg-slate-400 opacity-90 flex justify-center items-center">
+    <div className="absolute w-full h-full bg-slate-400 opacity-90 flex flex-col justify-center items-center">
       <button
         onClick={(e) => {
           handleFlag('game');
         }}
-        className="text-3xl font-bold text-white"
+        className="text-3xl font-bold text-white border-black"
       >
         start
       </button>
+      <div className="text-center">
+        <p>{word}</p>
+        <button onClick={handleClick}>초성 바꾸기</button>
+      </div>
     </div>
   );
 }
